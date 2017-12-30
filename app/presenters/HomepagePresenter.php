93e554fcd83b320cb30befa5468872cb31cfc4d9
Jakub Bouček <pan@jakubboucek.cz>
2017-12-30 06:46:08 +0100

    Testing ConfigManager

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,15 +2,35 @@
 
 namespace App\Presenters;
 
+use App\Model\ConfigManager;
+use Nette\Utils\Random;
+use Tracy\Debugger;
+
 class HomepagePresenter extends BasePresenter
 {
-    public function __construct()
+    /**
+     * @var ConfigManager
+     */
+    private $config;
+
+
+    /**
+     * HomepagePresenter constructor.
+     * @param ConfigManager $config
+     */
+    public function __construct(ConfigManager $config)
     {
         $this->isHp = true;
+        $this->config = $config;
     }
 
 
+    /**
+     * @throws \Nette\Utils\JsonException
+     */
     public function renderDefault()
     {
+        Debugger::barDump($this->config->get('a'));
+        $this->config->set('x', Random::generate());
     }
 }
