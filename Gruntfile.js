93816bf9f7c95e3270c574cab6d697b26fb84d26
Jakub Bouček <pan@jakubboucek.cz>
2018-01-17 05:22:23 +0100

    Move frontend dependencies (JS+CSS) from CDN do local + Add dependencies for Ublaboo Datagrid to admin + Update setup instructions in README.md

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'bower_components/bootstrap/fonts/', src: ['**'], dest: 'www/fonts/'},
+                ],
+            },
+        },
         less: {
             main: {
                 options: {
@@ -15,6 +22,14 @@ module.exports = function(grunt) {
                         'www/plugins/slick/slick.css',
                         'assets/less/main.less',
                         'assets/css/flash.css'
+                    ],
+                    'www/css/admin.css': [
+                        'assets/bower_components/bootstrap/dist/css/bootstrap.css',
+                        'assets/bower_components/happy/dist/happy.css',
+                        'assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css',
+                        'assets/bower_components/ublaboo-datagrid/assets/dist/datagrid.css',
+                        'assets/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.css',
+                        'assets/bower_components/bootstrap-select/dist/css/bootstrap-select.css',
                     ]
                 }
             }
@@ -34,16 +49,27 @@ module.exports = function(grunt) {
                         'vendor/nette/forms/src/assets/netteForms.js'
                     ],
                     'www/js/admin.js': [
-                        'assets/js/jquery-3.1.1.js',
-                        'vendor/nette/forms/src/assets/netteForms.js'
+                        'assets/bower_components/jquery/dist/jquery.js',
+                        'assets/bower_components/nette-forms/src/assets/netteForms.js',
+                        'assets/bower_components/nette.ajax.js/nette.ajax.js',
+                        'assets/bower_components/happy/dist/happy.js',
+                        'assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
+                        'assets/bower_components/jquery-ui-sortable/jquery-ui-sortable.js',
+                        'assets/bower_components/ublaboo-datagrid/assets/dist/datagrid.js',
+                        'assets/bower_components/ublaboo-datagrid/assets/dist/datagrid-instant-url-refresh.js',
+                        'assets/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.js',
+                        'assets/bower_components/bootstrap/dist/js/bootstrap.js',
+                        'assets/bower_components/bootstrap-select/dist/js/bootstrap-select.js',
+                        'assets/bower_components/clipboard/dist/clipboard.js'
                     ]
                 }
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['less', 'uglify']);
+    grunt.registerTask('default', ['copy', 'less', 'uglify']);
 };
--- README.md
+++ README.md
@@ -13,17 +13,27 @@ Po stažení repozitáře naisntalujte závislosti:
 
 Vytvořte soubor `app/config/config.local.neon` (není verzován v Gitu). Může být i prázdný.
 
+Úpravy JS a CSS
+---------------
+
 Pro úpravy JavaScriptových souborů a nebo stylů je potřeba nainstalovat závislosti pro generátor:
 
+    npm install -g bower
     npm install
+    bower install
     
-Po úpravě souborů v `assets/` zavolejte `grunt` a vygenerují se soubory:
+Po úpravě souborů v `assets/` zavolejte:
+
+    grunt
+
+a vygenerují se soubory:
 - `www/js/main.js`
 - `www/js/admin.js`
 - `www/css/main.css`
+- `www/css/admin.css`
 
-které obsahují veškeré scripty a styly stránek (kromě admin sekce, ta využívá css z oficiálních CDN). Tyto soubory jsou
-součástí repozitáře, takže je lze rovnou použít. 
+které obsahují veškeré scripty a styly stránek. Tyto soubory jsou součástí repozitáře, takže je lze
+rovnou použít. 
  
 
 Spuštění webového serveru
@@ -35,13 +45,20 @@ Spusťte Docker
 Na stránce `http://localhost:8000` by se měl objevit aktuální web.
 
 
-Požadavky
----------
+Požadavky pro běh
+-----------------
 
 PHP 5.6 nebo vyšší, Mysql 5 nebo vyšší, Git, Unzip. 
+
+
+Požadavky pro vývoj
+-----------------
+
+Composer, NPM 
 
 
 License
 -------
 - Web: The MIT License (MIT)
 - Nette: New BSD License or GPL 2.0 or 3.0
+- Další závislosti podle zveřejněných licení
