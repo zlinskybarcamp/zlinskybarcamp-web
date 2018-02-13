417d49f1d0029bf4883fb7cda990ac8ce3ff4387
Jakub Bouček <pan@jakubboucek.cz>
2018-02-13 09:02:11 +0100

    Wordpress PostReader - codestyle

--- app/components/Feed/FeedFactory.php
+++ app/components/Feed/FeedFactory.php
@@ -12,12 +12,19 @@ class FeedFactory
     private $postReader;
 
 
+    /**
+     * FeedFactory constructor.
+     * @param WordpressPostReader $postReader
+     */
     public function __construct(WordpressPostReader $postReader)
     {
         $this->postReader = $postReader;
     }
 
 
+    /**
+     * @return FeedControl
+     */
     public function create()
     {
         return new FeedControl($this->postReader);
